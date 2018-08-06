581e6062983020ad6cd6cb366b0ea838fc5f03c2
Elmqvist <Hilding.Elmqvist@Mogram.net>
2018-08-06 09:04:32 +0200

    Added test model for spatial discretization.

--- test/runsimulationtests.jl
+++ test/runsimulationtests.jl
@@ -14,6 +14,7 @@ include("models/TestEquations.jl")
 include("models/TestElectrical.jl")
 include("models/TestFilter.jl")
 include("models/TestArrayOfComponents.jl")
+include("models/TestSpatialDiscretization.jl")
 
 #include("models/TestCoupledInertias.jl")
 #include("models/TestPendulum.jl")
