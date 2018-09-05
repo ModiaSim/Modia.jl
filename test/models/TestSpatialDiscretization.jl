1e00525a8aad66622b493edade5acd3bbe4b5b51
Elmqvist <Hilding.Elmqvist@Mogram.net>
2018-09-05 22:38:14 +0200

    Addded model showing how to mix differential and algebraic elements in an array.

--- test/models/TestSpatialDiscretization.jl
+++ test/models/TestSpatialDiscretization.jl
@@ -16,4 +16,21 @@ end
 result = simulate(SpatialDiscretization, 1)
 plot(result, "u")
 
+
+splice(uodd, ueven) = [if isodd(i); uodd[div(i,2)+1] else ueven[div(i,2)] end for i in 1:length(ueven)+length(uodd)] 
+
+@model SpatialDiscretization2 begin
+    u = Var(size=(5,))
+    ueven = Var(start=[1,2])
+    uodd = Var(size=(3,))
+    @equations begin
+      u = splice(uodd, ueven)
+      der(ueven) = - u[[2,4]] + u[[1,3]]
+      uodd = ones(3)*time
+    end
+end
+
+result = simulate(SpatialDiscretization2, 1, logTranslation=true, storeEliminated=false)
+plot(result, "ueven")
+
 end
