50447d4b41a4e34aef0641324d973b0fe1067fc9
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2021-09-23 18:58:39 +0200

    Fixed wrong return argument of linearize! in test TestFirstOrder2.jl

--- test/TestFirstOrder2.jl
+++ test/TestFirstOrder2.jl
@@ -43,7 +43,7 @@ result3 = get_result(firstOrder, extraNames=["y"])
 
 # Linearize
 println("\n... Linearize at stopTime = 0 and 10:")
-A_0  = linearize!(firstOrder, analytic = true)
+(A_0, x0) = linearize!(firstOrder, analytic = true)
 (A_10, x_10) = linearize!(firstOrder, stopTime=10, analytic = true) 
 (A_10_numeric, x_10_numeric) = linearize!(firstOrder, stopTime=10, analytic=false) 
 xNames = get_xNames(firstOrder)
