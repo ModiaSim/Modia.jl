a68e24116bddb2d493d4cc9f9f1a06615ff05dab
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2022-02-20 21:12:45 +0100

    TestFilterCircuit slightly changed in order that it simulates with current ModiaLang

--- test/TestFilterCircuit.jl
+++ test/TestFilterCircuit.jl
@@ -47,15 +47,16 @@ plot(filterCircuit, [("R.v", "C.v"), ("R.R", "ground.p.i")], figure=1)
 
 
 # Simulate with lower precision
-filterCircuitLow = @instantiateModel(FilterCircuit, FloatType = Float32)
+filterCircuitLow = @instantiateModel(FilterCircuit, unitless=true, FloatType = Float32)  # unitless=true required, since Float32 not yet supported, if units are present
 simulate!(filterCircuitLow, RK4(), adaptive=false, stopTime=10.0, interval=0.01, 
-          merge = Map(R = Map(R = 5u"Ω"), C = Map(v = 3.0u"V")),
+          merge = Map(R = Map(R = 5.0), C = Map(v = 3.0)),                              #    merge = Map(R = Map(R = 5u"Ω"), C = Map(v = 3.0u"V")),
           requiredFinalStates = Float32[7.4248414])
 plot(filterCircuitLow, [("R.v", "C.v"), ("R.R", "ground.p.i")], figure=2)
 
 
 # Simulate with DAE integrator
 println("\n... Simulate with DAE integrator")
-simulate!(filterCircuit, IDA(), stopTime = 10, merge = Map(R = Map(R = 5u"Ω"), C = Map(v = 3.0u"V")), requiredFinalStates = [7.424843902110655]) 
-          
+filterCircuit2 = @instantiateModel(FilterCircuit) # Without a new @instantiateModel, this fails with a type error (will be fixed at a later time instant)
+simulate!(filterCircuit2, IDA(), stopTime = 10, merge = Map(R = Map(R = 5u"Ω"), C = Map(v = 3.0u"V")), requiredFinalStates = [7.424843902110655]) 
+    
 end
