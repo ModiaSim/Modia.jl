39e4315b77a3f2afe2297a00b2bad08ad5a92a56
Andrea Neumayr <andrea.neumayr@dlr.de>
2026-02-16 14:08:04 +0100

    update version number

--- src/Modia.jl
+++ src/Modia.jl
@@ -9,8 +9,8 @@ Main module of Modia.
 module Modia
 
 const path = dirname(dirname(@__FILE__))   # Absolute path of package directory
-const Version = "0.12.2"
-const Date = "2026-02-13"
+const Version = "0.12.3"
+const Date = "2026-02-16"
 const modelsPath = joinpath(Modia.path, "models")
 
 print(" \n\nWelcome to ")
